0e3557e8786d7630beb53efced6750471cf2448a
zahralmosawi <zalmosawii65@gmail.com>
2025-09-02 19:03:39 +0300

    added signup form and view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("auth/signup/", views.SignUpView.as_view(), name="signup"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import User, StudentProfile, ProgressLog
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import View, CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 def is_tracker(u):
     return u.role == u.Role.TRACKER
@@ -10,3 +14,18 @@ def is_student(u):
 
 def home(request):
     return render(request, 'home.html')
+
+class SignUpView(View):
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, 'registration/sign-up.html', {'form': form})
+    
+    def post(self, request):
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('login')
+        
+        return render(request, 'registration/sign-up.html', {'form': form})
