e074085f4cc97f20942a2b655ed324d60df34d64
zahralmosawi <zalmosawii65@gmail.com>
2025-09-08 09:47:45 +0300

    updated urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,15 +5,17 @@ from django.urls import reverse_lazy
 
 urlpatterns = [
     path("auth/login/", views.CustomLoginView.as_view(), name="login"),
+    
     path('tracker/add-student/', views.AddStudentView.as_view(), name='add_student'),
     path('tracker/students-list/', views.ListStudentsView.as_view(), name='students_list'), 
     path('tracker/student/<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
     path('tracker/student/<int:pk>/edit/', views.UpdateStudentView.as_view(), name='edit_student'),
     path('tracker/<int:pk>/delete/', views.DeleteStudentView.as_view(), name='delete_student'),
-    path('student/dashboard/', views.StudentDashboardView.as_view(), name='student_dashboard'),
     path('tracker/student/<int:student_id>/progress-log/create/', views.AddProgressLogView.as_view(), name='add_progress_log'),
     path('tracker/student/<int:pk>/progress-log/view/', views.ProgressLogDetailView.as_view(), name='progress_log_detail'),
     path('tracker/progress-log/<int:pk>/edit/', views.UpdateProgressLogView.as_view(), name='edit_progress_log'),
     path('tracker/student/<int:student_id>/log/<int:month_number>/', views.ListProgressLogsView.as_view(), name='monthly_logs'),
+
+    path('student/dashboard/', views.StudentDashboardView.as_view(), name='student_dashboard'),
     path('student/change-password/', auth_views.PasswordChangeView.as_view(template_name='student/change_password.html',success_url=reverse_lazy('student/dashboard/')), name='change_password'),
 ]
