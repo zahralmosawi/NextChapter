5d4f399167b810d2aa05f290464ac8de09d7edc7
zahralmosawi <zalmosawii65@gmail.com>
2025-09-08 13:24:03 +0300

    added event form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User, StudentProfile, ProgressLog
+from .models import User, StudentProfile, ProgressLog, Event
 
 class StudentProfileForm(forms.ModelForm):
     email = forms.EmailField(required=True)
@@ -11,3 +11,9 @@ class ProgressLogForm(forms.ModelForm):
     class Meta:
         model = ProgressLog
         fields = ['date', 'interaction_type', 'status', 'tracker_name', 'comment']
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['title', 'date', 'description']
+        widgets = {'date': forms.DateTimeInput(attrs={'type': 'datetime-local'})}
